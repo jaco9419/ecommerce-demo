81646d7a16ee37efb97198b141addb3f6fb649aa
jaco9419 <jaco.9419@gmail.com>
2021-01-14 10:58:10 -0300

    Start header

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+npx create-react-app\
+npm install react-router-dom\
+rfce (shortcut for a component template)\
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <nav className="header">
+            <img
+                className="header__logo"
+                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                alt="amazon logo"
+            />
+        </nav>
+    );
+}
+
+export default Header;
