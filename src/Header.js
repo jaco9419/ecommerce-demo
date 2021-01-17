09bbc2c2b0e3d4ddef9c366f36a05e058c98af4a
jaco9419 <jaco.9419@gmail.com>
2021-01-16 23:38:51 -0300

    Build Home and Products Components

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
-import './Header.css';
+import './style/Header.css';
 
 function Header() {
     return (
